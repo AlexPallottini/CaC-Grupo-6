6a5f9392bd4b9eb68e29d778003b4b8709318a81
AlexPallottini <alexwaltin@yahoo.com.ar>
2023-04-30 23:51:59 -0300

    Multiple changes
    Changed name of products html since it's not an e commerce page
    Added examples of wines
    Started working on a soon to rename about us page for now called experience
    Worked on index page

--- js/productManagment.js
+++ js/productManagment.js
@@ -7,39 +7,39 @@ function populateProductList()
     const productsList = 
     [
         {
+            "productName" : "Mariano Di Paola",
+            "productDescription" : "Presenta un color muy intenso con matices violáceos, en la nariz aparecen notas frutales con toques de especias, y una leve sensación floral. En la boca es un vino de gran concentración y persistencia, muy especiado y frutal, con taninos muy firmes, pero a su vez muy suaves.",
+            "productImage" : "data/img/vino-mariano-di-paola.jpeg"
+        },
+        {
+            "productName" : "Montebaco Crianza 2014 (Ribera del Duero, España)",
+            "productDescription" : "Montebaco Crianza presenta en nariz un predominio de frutos del bosque, especiados, minerales y maderas finas, y en boca se aprecian sus taninos, vivos aunque amables y elegantes. Un vino que no defraudará a los seguidores de la Ribera del Duero. Montebaco Crianza es ideal para acompañar con bistec a la pimienta, conejo a la brasa, queso manchego curado, quesos viejos.",
+            "productImage" : "data/img/vino-montebaco-crianza.jpg"
+        },
+        {
             "productName" : "Nombre de producto",
-            "productDescription" : "Descripción",
+            "productDescription" : "Descripción3",
             "productImage" : "data/img/vinoejemplo.png"
         },
         {
             "productName" : "Nombre de producto",
-            "productDescription" : "Descripción",
+            "productDescription" : "Descripción4",
             "productImage" : "data/img/vinoejemplo.png"
         },
         {
             "productName" : "Nombre de producto",
-            "productDescription" : "Descripción",
+            "productDescription" : "Descripción5",
             "productImage" : "data/img/vinoejemplo.png"
         },
         {
             "productName" : "Nombre de producto",
-            "productDescription" : "Descripción",
-            "productImage" : "data/img/vinoejemplo.png"
-        },
-        {
-            "productName" : "Nombre de producto",
-            "productDescription" : "Descripción",
-            "productImage" : "data/img/vinoejemplo.png"
-        },
-        {
-            "productName" : "Nombre de producto",
-            "productDescription" : "Descripción",
+            "productDescription" : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit odio eum commodi porro, illo voluptatem, modi similique pariatur vel doloribus dolores ipsa iste perferendis, cumque soluta ullam molestiae veritatis dicta.",
             "productImage" : "data/img/vinoejemplo.png"
         }
     ];
 
     for (let index = 0; index < productsList.length; index++) {
-
+    
         //Children variables
         let child = document.createElement("li");
         let imageChild = document.createElement("img");
@@ -47,12 +47,12 @@ function populateProductList()
         let paragraphChild = document.createElement("p");
 
         //Set children variables data
-        imageChild.src = productsList[0].productImage;
+        imageChild.src = productsList[index].productImage;
         imageChild.alt = "product-image";
 
-        headerChild.innerText = productsList[0].productName;
+        headerChild.innerText = productsList[index].productName;
         
-        paragraphChild.innerText = productsList[0].productDescription;
+        paragraphChild.innerText = productsList[index].productDescription;
 
         child.className = "product-item";
         
